7a8609b5b3dd8513805401c3a767b61721034453
tabzhangjx <tabzhangjx@outlook.com>
2017-09-19 22:27:42 +0800

    ud

--- poj/luvletter.cpp
+++ poj/luvletter.cpp
@@ -1,29 +1,28 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main(){
-	int T;
-	cin>>T;
-	char luvletter[10]={"luvletter"};
-	char check[100010]={'\0'};
-	for(int i=0;i<T;i++){
-		int NUM=0;
-		cin.getline(check,100001);
-        int j=0;
-		while(j!=-1){
-			for(int k=0;check[k]!='\0';k++){
-				if(check[k]==luvletter[j]&&j==8){
-					NUM=NUM+1;
-					j=0;
-				}
-				else if(check[k]==luvletter[j]&&j!=8){
-					j++;
-				}
-				else if(check[k]=='\0'){
-					j=-1;
-				}
-				cout<<NUM<<endl;
+int main() {
+	int T = 0;
+	cin >> T;
+	char luvletter[10] = { "luvletter" };
+	char check[100001] = { '\0' };
+	char temp;
+	int NUM = 0;
+	int flag = 0;
+	for (int i = 0; i < T; i++) {
+		cin >> temp;
+		cin.getline(check+1, 100001);
+		check[0] = temp;
+		for (int j = 0; check[j] != '\0'; j++) {
+			if (check[j] == luvletter[flag] && flag == 8) {
+				NUM = NUM + 1;
+				flag = 0;
+			}
+			else if (check[j] == luvletter[flag] && flag != 8) {
+				flag++;
 			}
 		}
+		cout << NUM << endl;
+		NUM = 0;
 	}
 	return 0;
 }
