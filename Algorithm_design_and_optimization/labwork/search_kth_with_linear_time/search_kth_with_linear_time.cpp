530d58fbc1f9c0d127425b0b99611ea8b11556b5
tabzhangjx <tabzhangjx@outlook.com>
2017-11-05 09:53:41 +0800

    a

--- Algorithm_design_and_optimization/labwork/search_kth_with_linear_time/search_kth_with_linear_time.cpp
+++ Algorithm_design_and_optimization/labwork/search_kth_with_linear_time/search_kth_with_linear_time.cpp
@@ -2,16 +2,55 @@
 #include <iostream>
 using namespace std;
 
+int 
 int searchlabel(int* a,int n,int k){
-
-
-}
-
-void sortbylabel(int*a,int lable,int n){
 	
+
 }
 
-int sortfew(int*a,int m,int n){
+int sortbylabel(int* a,int lable,int n){
+	int tem=a[label];
+	int mark=label;
+	int flag=0;
+	int record=0;
+	for(int i=0,j=n;i!=j;){
+		if(flag==0){
+			if(i<mark){
+				flag=1;
+				continue;
+			}
+			if(a[i]<=tem){
+				i++;
+			}
+			else{
+				a[mark]=a[i];
+				mark=i;
+				i++;
+				record=i;
+				flag=1;
+			}
+		}
+		else{
+			if(j>mark){
+				flag=0;
+				continue;
+			}
+			if(a[i]>=tem){
+				j--;
+			}
+			else{
+				a[mark]=a[j];
+				mark=j;
+				j--;
+				record=j;
+				flag=0;
+			}
+		}
+	}
+	return record;
+}
+
+int sortfew(int* a,int m,int n){
 	int min;
 	int mark=0;
 	for(int k=0;k<(n-m);k++){
