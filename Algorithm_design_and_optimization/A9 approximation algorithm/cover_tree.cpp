7295ecfaacd1ffa9cbb3a1634fac62b0c81d7dcc
tabzhangjx <tabzhangjx@outlook.com>
2017-11-17 23:14:44 +0800

    a

---
+++ Algorithm_design_and_optimization/A9 approximation algorithm/cover_tree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+	Node* parent;
+	Node* lson;
+	Node* rson;
+
+}
+
+int main(){
+	int n=0;
+	int T=0;
+	bool 
+
+
+
+
+	return 0;
+}
