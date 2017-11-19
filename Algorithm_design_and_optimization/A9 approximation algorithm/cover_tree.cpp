c0fc99cfcd6a49093392173e7ffc47b0fdebf262
tabzhangjx <tabzhangjx@outlook.com>
2017-11-20 00:03:57 +0800

    a

--- Algorithm_design_and_optimization/A9 approximation algorithm/cover_tree.cpp
+++ Algorithm_design_and_optimization/A9 approximation algorithm/cover_tree.cpp
@@ -1,21 +1,74 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
-class Node{
+struct nodeinf {
+	int numsons;
+	int *sons;
+	bool visited;
+};
+class cmp {
 public:
-	Node* parent;
-	Node* lson;
-	Node* rson;
+	bool operator()(Node *x, Node *y) { return true; };
+};
+priority_queue <Node*, vector<Node*>, cmp> Search;
 
+class Node {
+public:
+	Node *parent;
+	int num_sons;
+	Node** sons;
+	bool visited;
+	Node(Node *c) { parent = c; num_sons = 0; sons = NULL; visited = 0; };
+	~Node() {};
+	void Buildnodes(nodeinf *c, int i);
+	void dfs();
+};
+
+void Node::dfs() {
+	Search.push(this);
+}
+void Node::Buildnodes(nodeinf *c, int i) {
+	this->num_sons = c[i].numsons;
+	sons = new Node*[num_sons];
+	for (int j = 0; j < c[i].numsons; j++) {
+		sons[j] = new Node(this);
+		sons[j]->Buildnodes(c, c->sons[j]);
+	}
 }
 
-int main(){
-	int n=0;
-	int T=0;
-	bool 
+class Commontree {
+public:
+	Node *root;
+	Commontree(Node *c) { root = c; };
+	Commontree() { delete root; };
+	void DFS();
+};
 
+void Commontree::DFS() {
 
-
-
+}
+int main() {
+	int n = 0;
+	cin >> n;
+	nodeinf *nodes = new nodeinf[n];
+	for (int i = 0; i < n; i++) {
+		nodes[i].visited = false;
+		cin >> nodes[i].numsons;
+		if (nodes[i].numsons == 0) {
+			nodes[i].sons = NULL;
+		}
+		else {
+			nodes[i].sons = new int[nodes[i].numsons];
+		}
+		for (int j = 0; j < nodes[i].numsons; j++) {
+			cin >> nodes[i].sons[j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		delete[]nodes[i].sons;
+	}
+	delete[]nodes;
 	return 0;
 }
