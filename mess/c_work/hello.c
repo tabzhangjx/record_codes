6dd0a32026e25339c67972063fc0065caecceb98
tabzhangjx <tabzhangjx@outlook.com>
2017-12-21 20:00:28 +0800

    a

--- mess/c_work/hello.c
+++ mess/c_work/hello.c
@@ -3,7 +3,14 @@
 
 int main(){
 
-
+	unsigned short a=67575745475476;
+	int b;
+	printf("%d \n",a);
+	printf("%d \n",b);
+	printf("%d \n",b=a);
+	printf("%d \n",b);
+	b=a;
+	printf("%d \n",b);
 	printf("hello world.");
 
 
