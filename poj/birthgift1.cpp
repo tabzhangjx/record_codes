f59847e99db74ca69889345e31447fdbe0bd6d5f
tabzhangjx <tabzhangjx@outlook.com>
2018-02-26 00:12:25 +0800

    a

--- poj/birthgift1.cpp
+++ poj/birthgift1.cpp
@@ -1,45 +1,29 @@
 #include<bits/stdc++.h>
-
 #define pa pair<int,int>
 #define ll long long
 #define inf 10000005
-
 using namespace std;
-
 inline int read(){
-    int x=0,f=1;
-    char ch=getchar();
-    while(ch<'0'||ch>'9'){
-        if(ch=='-') f=-1;
-        ch=getchar();
-    }
-    while(ch>='0'&&ch<='9'){
-        x=x*10+ch-'0';
-        ch=getchar();
-    }
+    int x=0,f=1;char ch=getchar();
+    while (ch<'0'||ch>'9'){if (ch=='-') f=-1;ch=getchar();}
+    while (ch>='0'&&ch<='9'){x=x*10+ch-'0';ch=getchar();}
     return x*f;
 }
-
 struct cmp{
     bool operator()(pa a,pa b){
         return abs(a.first)>abs(b.first);
     }
-}
-
+};
 priority_queue<pa,vector<pa>,cmp>q;
-
 int n,m,tot,a[100005],sum,ans,nxt[100005],pre[100005];
-
 bool mark[100005];
-
 void del(int x){
     mark[x]=1;
     pre[nxt[x]]=pre[x];
     nxt[pre[x]]=nxt[x];
 }
-
 int main(){
-	n=read();m=read();tot=1;
+    n=read();m=read();tot=1;
     for (int i=1;i<=n;i++)
     {
         int x=read();
