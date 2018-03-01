e3a321ddbdafbdf83530b03f15cf8cb4aca8bf3e
tabzhangjx <tabzhangjx@outlook.com>
2018-03-01 23:34:44 +0800

    a

--- poj/manycircles.cpp
+++ poj/manycircles.cpp
@@ -1,65 +1,87 @@
-//一：跑概率，枚举若干个点，检测所有点都在半径二之内；
-//二：
-
-#include <iostream>
-
-struct node{
-	int x=0;
-	int y=0;
-};
-
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+typedef double ld;
+const ld _0=1e-6l;
+const int N=100010;
+int n;
+ld x[N],y[N],r[N];
+ld L,R;
+int read(){
+    int x=0,c=getchar(),f=1;
+    while(c>57||c<48){if(c=='-')f=-1;c=getchar();}
+    while(c>47&&c<58)x=x*10+c-48,c=getchar();
+    return x*f;
+}
+void get(int id,ld X,ld&a,ld&b){
+    X-=x[id];
+    X=sqrt(r[id]*r[id]-X*X);
+    a=y[id]-X;
+    b=y[id]+X;
+}
+bool chk(ld X,int a,int b){
+    if(fabs(X-x[a])>r[a]||fabs(X-x[b])>r[b])return 0;
+    ld a1,a2,b1,b2;
+    get(a,X,a1,a2);
+    get(b,X,b1,b2);
+    return a2>b1&&a1<b2;
+}
+void chk(int a,int b){
+    ld xd=x[b]-x[a],yd=y[b]-y[a];
+    ld d=sqrt(xd*xd+yd*yd);
+    if(d+_0>r[a]+r[b]){
+        puts("NO");
+        exit(0);
+    }
+    ld s=(r[a]+r[b]+d)/2.;
+    ld h=2*sqrt(s*(s-r[a])*(s-r[b])*(s-d))/d;
+    ld m=x[a]+xd*sqrt(r[a]*r[a]-h*h)/d,c=h*fabs(yd)/d;
+    if(d*d+r[a]*r[a]<r[b]*r[b])m=x[a]*2-m;
+    ld lx=m-c,rx=m+c;
+    if(lx>x[a]-r[a]&&chk(x[a]-r[a]+_0,a,b))lx=x[a]-r[a];
+    if(lx>x[b]-r[b]&&chk(x[b]-r[b]+_0,a,b))lx=x[b]-r[b];
+    if(rx<x[a]+r[a]&&chk(x[a]+r[a]-_0,a,b))rx=x[a]+r[a];
+    if(rx<x[b]+r[b]&&chk(x[b]+r[b]-_0,a,b))rx=x[b]+r[b];
+    if(lx>L)L=lx;
+    if(rx<R)R=rx;
+}
 int main(){
-	int n=0;
-	std::cin>>n;
-    node point[100000];
+    n=read();
     for(int i=0;i<n;i++){
-    	std::cin>>point[i].x;
-    	std::cin>>point[i].y;
+        x[i]=read();
+        y[i]=read();
+        r[i]=read();
     }
-    node mark[100000];
-    int marki[4];
-    marki[0]=point[0].x;
-    int flag=0;
+    L=x[0]-r[0],R=x[0]+r[0];
     for(int i=1;i<n;i++){
-    	if(point[i].x>marki[0]){
-    		marki[0]=point[i].x;
-    		flag=i;
-    	}
+        if(L<x[i]-r[i])L=x[i]-r[i];
+        if(R>x[i]+r[i])R=x[i]+r[i];
     }
-    marki[1]=point[0].x;
-    flag=0;
-    for(int i=1;i<n;i++){
-    	if(point[i].x<marki[1]){
-    		marki[1]=point[i].x;
-    		flag=i;
-    	}
+    while(1){
+        if(L+_0>R){
+            puts("NO");
+            return 0;
+        }
+        ld M=(L+R)/2.;
+        ld y1,y2,a1,a2;
+        get(0,M,y1,y2);
+        for(int i=1;i<n;i++){
+            get(i,M,a1,a2);
+            if(a1>y1)y1=a1;
+            if(a2<y2)y2=a2;
+            if(y1+_0>y2){
+                for(int j=0;j<i;j++){
+                    get(j,M,y1,y2);
+                    if(y1>=a2-_0||y2<=a1+_0){
+                        chk(i,j);
+                        goto re;
+                    }
+                }
+            }
+        }
+        puts("YES");
+        return 0;
+        re:;
     }
-    marki[2]=point[0].y;
-    flag=0;
-    for(int i=1;i<n;i++){
-    	if(point[i].y>marki[2]){
-    		marki[2]=point[i].y;
-    		flag=i;
-    	}
-    }
-    marki[3]=point[0].y;
-    flag=0;
-    for(int i=1;i<n;i++){
-    	if(point[i].y<marki[3]){
-    		marki[3]=point[i].y;
-    		flag=i;
-    	}
-    }
-    int x0=(marki[0]+marki[1])/2;
-    int y0=(marki[2]+marki[3])/2;
-    int total;
-    for(int i=0;i<n;i++){
-    	total=(point[i].x-x0)*(point[i].x-x0)+(point[i].y-y0)*(point[i].y-y0);
-    	if(total>1){
-    		std::cout<<"NO";
-    		return 0;
-    	}
-    }
-    std::cout<<"yes";
-    return 0;
 }
