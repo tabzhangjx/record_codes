86bb2897a0b393a0ad320d0ae2d68c0a5d8ba7fe
tabzhangjx <tabzhangjx@outlook.com>
2018-03-08 23:17:55 +0800

    a

---
+++ luogu/maliao.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    string to_print1="                ********";
+    string to_print2="               ************";
+    string to_print3="               ####....#.";
+    string to_print4="             #..###.....##....";
+    string to_print5="             ###.......######              ###            ###";
+    string to_print6="                ...........               #...#          #...#";
+    string to_print7="               ##*#######                 #.#.#          #.#.#";
+    string to_print8="            ####*******######             #.#.#          #.#.#";
+    string to_print9="           ...#***.****.*###....          #...#          #...#";
+    string to_print10="           ....**********##.....           ###            ###";
+    string to_print11="           ....****    *****....";
+    string to_print12="             ####        ####";
+    string to_print13="           ######        ######";
+    string to_print14="##############################################################";
+    string to_print15="#...#......#.##...#......#.##...#......#.##------------------#";
+    string to_print16="###########################################------------------#";
+    string to_print17="#..#....#....##..#....#....##..#....#....#####################";
+    string to_print18="##########################################    #----------#";
+    string to_print19="#.....#......##.....#......##.....#......#    #----------#";
+    string to_print20="##########################################    #----------#";
+    string to_print21="#.#..#....#..##.#..#....#..##.#..#....#..#    #----------#";
+    string to_print22="##########################################    ############";
+    cout<<to_print1<<endl;
+    cout<<to_print2<<endl;
+    cout<<to_print3<<endl;
+    cout<<to_print4<<endl;
+    cout<<to_print5<<endl;
+    cout<<to_print6<<endl;
+    cout<<to_print7<<endl;
+    cout<<to_print8<<endl;
+    cout<<to_print9<<endl;
+    cout<<to_print10<<endl;
+    cout<<to_print11<<endl;
+    cout<<to_print12<<endl;
+    cout<<to_print13<<endl;
+    cout<<to_print14<<endl;
+    cout<<to_print15<<endl;
+    cout<<to_print16<<endl;
+    cout<<to_print17<<endl;
+    cout<<to_print18<<endl;
+    cout<<to_print19<<endl;
+    cout<<to_print20<<endl;
+    cout<<to_print21<<endl;
+    cout<<to_print22;
+}
