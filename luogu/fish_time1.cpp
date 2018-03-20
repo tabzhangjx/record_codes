20e39ce1b7b709417a5d6f7647e4685187299dea
tabzhangjx <tabzhangjx@outlook.com>
2018-03-20 09:22:11 +0800

    a

--- luogu/fish_time1.cpp
+++ luogu/fish_time1.cpp
@@ -7,10 +7,9 @@ int main(){
     sum+=250*5*(int)(n/7);
     n%=7;
     for(int i=n; i>0; i--){
+        if(x!=6&&x!=7&&x!=0) sum+=250;
         x=(x+1)%7;
-        if(x!=6&&x!=7) sum+=250;
     }
-    sum+=250;
     std::cout<<sum;
     return 0;
 }
