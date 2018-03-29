159784f80e703b095c7f5e0b80d95759bad65994
tabzhangjx <tabzhangjx@outlook.com>
2018-03-29 15:19:10 +0800

    a

--- luogu/oppe_num.cpp
+++ luogu/oppe_num.cpp
@@ -1,49 +1,83 @@
 #include <iostream>
 
 int label;
-int m[20], n[20], count1, count2;
-int read(){
-    char a;
-    std::cin>>a;
-    if('0'<=a&&a<='9'){
-        return a-'0'+1;
+int record;
+
+void oppo_print(char *a, int start, int end){
+    int length=end-start+1;
+    if(length==0){
+        std::cout<<'0';
+        return;
     }
-    else if(a=='.'){
-        label=1;
+    int label1=0;
+    for(int i=end;i>=start;i--){
+        if(a[i]!='0'){
+            std::cout<<a[i];
+            label1=1;
+        }
+        else if(a[i]=='0'&&label1==1) std::cout<<a[i];
     }
-    else if(a=='/'){
-        label=2;
-    }
-    else if(a=='%'){
-        label=3;
-    }
-    return 0;
+    if(label1==0) std::cout<<'0';
 }
 
-void cin_1(){
-    while(label==0){
-        int temp=read();
-        if(label==0){
-            m[count1]=temp;
-            count1++;
+void other_print(char *a, int start, int end){
+    for(int i=start;i<=end;i++){
+        if(a[i]=='0'){
+            start++;
         }
-        else if(label==1||label=2){
-            cin_2();
-            break;
-        }
-        else{
-            break;
-        }
-
+        else break;
     }
-
-
+    int length=end-start+1;
+    if(length==0){
+        std::cout<<'0';
+        return;
+    }
+    int label1=0;
+    for(int i=end;i>=start;i--){
+        if(a[i]!='0'){
+            std::cout<<a[i];
+            label1=1;
+        }
+        else if(a[i]=='0'&&label1==1) std::cout<<a[i];
+    }
+    if(label1==0) std::cout<<'0';
 }
+
 int main(){
-    label=0;
-    count1=0;
-    count2=0;
-    cin_1();
-
-
+    char num[30];
+    std::cin.getline(num,30);
+    label=-1;
+    int i=0;
+    while(num[i]!='\0'){
+        if(num[i]=='.'){
+            label=1;
+            record=i;
+        }
+        else if(num[i]=='/'){
+            label=3;
+            record=i;
+        }
+        else if(num[i]=='%'){
+            label=2;
+        }
+        i++;
+    }
+    if(label==-1){
+        oppo_print(num, 0, i-1);
+    }
+    else if(label==2){
+        oppo_print(num, 0, i-2);
+        std::cout<<'%';
+    }
+    else if(label==3){
+        oppo_print(num, 0, record-1);
+        std::cout<<num[record];
+        oppo_print(num, record+1, i-1);
+    }
+    else if(label==1){
+        oppo_print(num, 0, record-1);
+        std::cout<<num[record];
+        other_print(num, record+1, i-1);
+    }   
+    return 0;
 }
