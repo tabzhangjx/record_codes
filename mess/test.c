ba0f0c8773e9d58bf5e3f26d18de0ecbd7c10f21
tabzhangjx <tabzhangjx@outlook.com>
2018-05-15 21:41:13 +0800

    a

--- mess/test.c
+++ mess/test.c
@@ -1,19 +1,35 @@
 #include<stdio.h>
-int main()
-{
-  int n,i,j;
-  int aaa[8][8];
-  scanf("%d",&n);
-  for(i=0;i<n;i++)
-  {
-    for(j=0;j<n;j++)
-      scanf("%d",&aaa[i][j]);
+#include<string.h>
+
+double fun(int a[], int n, int *max, int *min){
+  int i, sum = 0;
+  *max = 0;
+  *min = 0;
+  int max1 = a[0];
+  int min1 = a[0];
+  for (i = 0; i < n; i++){
+    sum = sum + a[i];
+    if (a[i] > max1) { 
+      *max = i; 
+      max1 = a[i];
+    }
+    if (a[i] < min1) {
+      *min = i;
+      min1 = a[i];
+      }
   }
-  for(i=0;i<n;i++)
-  {
-    for(j=0;j<n;j++)
-      printf("%-5d",aaa[j][i]);
-    printf("\n");
+  return sum / n;
+}
+int main(){
+  int b[1000], o, i ;
+  int *ma, *mi;
+  scanf("%d", &o);
+  for (i = 0; i < o; i++){
+    scanf("%d", &b[i]);
   }
- return 0;
+  double ave=fun(b, o, ma, mi);
+  printf("%d/n", b[*ma]);
+  printf("%d/n", b[*mi]);
+  printf("%lf/n", ave);
+  return 0;
 }
